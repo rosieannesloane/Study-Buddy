dc72d224f03e4090635d052c767dc8a97dab1f7b
rosieannesloane <rosieannesloane@gmail.com>
2025-05-08 18:19:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,33 @@
 import streamlit as st
+from datetime import datetime
+import pandas as pd
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+
+data_df = pd.DataFrame(
+    {
+        "appointment": [
+            datetime(2024, 2, 5, 12, 30),
+            datetime(2023, 11, 10, 18, 0),
+            datetime(2024, 3, 11, 20, 10),
+            datetime(2023, 9, 12, 3, 0),
+        ]
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "appointment": st.column_config.DatetimeColumn(
+            "Appointment",
+            min_value=datetime(2023, 6, 1),
+            max_value=datetime(2025, 1, 1),
+            format="D MMM YYYY, h:mm a",
+            step=60,
+        ),
+    },
+    hide_index=True,
 )
